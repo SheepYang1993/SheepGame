9ed0d9801b5477a750a0924a4a96d51fefca8643
SheepYang1993 <332594623@qq.com>
2019-03-14 10:51:33 +0800

    Initial commit

---
+++ core/src/com/sheepyang/game/MyGame.java
@@ -0,0 +1,37 @@
+package com.sheepyang.game;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		Application.ApplicationType type = Gdx.app.getType();
+		String name = type.name();
+		System.out.println("平台：" + name);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
---
+++ desktop/src/com/sheepyang/game/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.sheepyang.game.desktop;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sheepyang.game.MyGame;
+
+import org.lwjgl.LWJGLUtil;
+
+public class DesktopLauncher {
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new MyGame(), config);
+    }
+}
